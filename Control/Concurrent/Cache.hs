a589afd035230bac555bde7aff570da6c9f8e591
Marcus Ofenhed <marcus@conditionraise.se>
2016-07-10 07:40:44 +0200

    Created seperate timeout types, specifically one that resets timer on read and one that doesn't

--- Control/Concurrent/Cache.hs
+++ Control/Concurrent/Cache.hs
@@ -1,10 +1,12 @@
-module Control.Concurrent.Cache (CachedData, fetch, createCache) where
+module Control.Concurrent.Cache (CachedData, fetch, createReadOnceCache, createTimedCache) where
 
 import Data.Maybe (isNothing)
 import Control.Concurrent (forkIO, threadDelay, killThread, MVar, modifyMVar_, readMVar, ThreadId, newMVar)
 import Control.Monad (when, liftM)
 
-data CachedData a = TimedCachedData (Int, (MVar (Maybe ThreadId, IO a, Maybe a))) | ReadOnceCachedData (MVar (Either (IO a) a))
+data Timeout = TimeSinceCreation Int | TimeSinceLastRead Int 
+
+data CachedData a = TimedCachedData (Timeout, (MVar (Maybe ThreadId, IO a, Maybe a))) | ReadOnceCachedData (MVar (Either (IO a) a))
 
 -- |Fetch data from a cache
 fetch :: CachedData a
@@ -32,28 +34,48 @@ fetch (TimedCachedData (timeout, mvar)) = go where
                                           Just x -> return $ mvar'
         go
       Just value' -> do 
-        modifyMVar_ mvar $ \(thread', action', value') -> do
-          when (not $ isNothing thread') $ let Just thread'' = thread' in killThread thread''
-          newThreadId <- forkIO $ do
-            threadDelay timeout
-            modifyMVar_ mvar $ \(_, action'', _) -> return (Nothing, action'', Nothing)
-          return (Just newThreadId, action', value')
+        modifyMVar_ mvar $ \mvar'@(thread', action', value') -> do
+          let newThread x = do threadDelay x
+                               modifyMVar_ mvar $ \(_, action'', _) -> return (Nothing, action'', Nothing)
+          case timeout of
+               TimeSinceLastRead time -> do
+                 when (not $ isNothing thread') $ let Just thread'' = thread' in killThread thread''
+                 newThreadId <- forkIO $ newThread time
+                 return (Just newThreadId, action', value')
+               TimeSinceCreation time -> do
+                 if (isNothing thread')
+                    then do newThread' <- forkIO $ newThread time
+                            return (Just newThread', action', value')
+                    else return mvar'
         return value'
 
 
 -- |Create a cache with a timeout from an (IO ()) function.
-createCache :: Int
+createReadOnceCache  :: IO (a)
+            -- ^ @Fetcher@, the function that returns the data which should
+            -- be cached. If @Timeout@ is not set to zero, this function
+            -- must be allowed to be called more than once.
+            -> IO (CachedData a)
+createReadOnceCache action = do
+  var <- newMVar $ Left action
+  return $ ReadOnceCachedData var
+
+-- |Create a cache with a timeout from an (IO ()) function.
+createTimedCache  :: Int
             -- ^ @Timeout@ in microseconds before the cache is erased, 0 to
             -- disable emptying of the cache
+            -> Bool
+            -- ^ @resetTimerOnRead@, if true the timeout will be reset
+            -- every time the cache is read, otherwise it will only be
+            -- reset when the cached value is set.
             -> IO (a)
             -- ^ @Fetcher@, the function that returns the data which should
             -- be cached. If @Timeout@ is not set to zero, this function
             -- must be allowed to be called more than once.
             -> IO (CachedData a)
-createCache 0 action = do
-  var <- newMVar $ Left action
-  return $ ReadOnceCachedData var
-
-createCache timeout action = do
+createTimedCache timeout resetOnRead action = do
   var <- newMVar (Nothing, action, Nothing)
-  return $ TimedCachedData (timeout, var)
+  let timeout' = if resetOnRead
+                   then TimeSinceLastRead timeout
+                   else TimeSinceCreation timeout
+  return $ TimedCachedData (timeout', var)
